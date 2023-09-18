e7817b89ccea36abed71a75b2cf8c1d616b77971
EdBispoDev <edbispo.dev@gmail.com>
2023-09-18 15:54:21 -0300

    classe contaBancaria finalizado com sucesso

---
+++ sistema-bancario/src/programa/ContaBancaria.java
@@ -0,0 +1,85 @@
+package programa;
+
+
+public class ContaBancaria {
+
+    private static int contadorDeContas = 1;
+    private int numeroConta;
+    private Cliente cliente;
+    private Double saldo = 0.0;
+
+    public ContaBancaria(Cliente cliente) {
+        this.numeroConta = numeroConta;
+        this.cliente = cliente;
+        contadorDeContas += 1;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String toString() {
+        return "\nNumero da conta: " + this.getNumeroConta() +
+                "\nNome: " + this.cliente.getNome() +
+                "\nCpf: " + this.cliente.getCpf() +
+                "\nEmail: " + this.cliente.getEmail() +
+                "\nSaldo: " + Utils.doubleToString(this.getSaldo()) +
+                "\n";
+    }
+
+    public void depositar(Double valor) {
+        if (valor > 0) {
+            setSaldo(getSaldo() + valor);
+            System.out.println("Seu depósito foi realizado com sucesso");
+        } else {
+            System.out.println("Não foi possível realizar o depósito");
+        }
+    }
+
+    public void sacar(Double valor) {
+        if (valor > 0 && this.getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+            System.out.println("Saque realizado com sucesso");
+        } else {
+            System.out.println("Não foi possível realizar o saque");
+        }
+    }
+
+
+    public void transferir(ContaBancaria contaParaDeposito, Double valor) {
+        if(valor > 0 && this.getSaldo() >= valor){
+            setSaldo(getSaldo() - valor);
+
+            contaParaDeposito.saldo = contaParaDeposito.getSaldo() + valor;
+            System.out.println("Transfência realizada com sucesso");
+        }else {
+            System.out.println("Não foi possível realizar a transferência");
+        }
+    }
+
+
+}
+
+
+
+
